eaf8c6b42453e70df18ab3e3dd083c0c3a14c7db
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-30 12:51:21 -0600

    feat: agregando get mostrar todos los empleados

---
+++ WebApiEmpleados/Controllers/EmpleadoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiEmpleados.Models;
+
+namespace WebApiEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadoController : ControllerBase
+    {
+        private readonly EmpleadosDbcrudContext _context;
+
+        public EmpleadoController(EmpleadosDbcrudContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Empleado>>> Get() => await _context.Empleados.ToListAsync();
+
+    }
+}
