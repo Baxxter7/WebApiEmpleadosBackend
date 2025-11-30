3f6efedd84932134389497537f999c10aafa629d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-30 12:50:36 -0600

    feat: configurando swagger y sql server

--- WebApiEmpleados/Program.cs
+++ WebApiEmpleados/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiEmpleados.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EmpleadosDbcrudContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Swagger UI tradicional
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -11,7 +24,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
