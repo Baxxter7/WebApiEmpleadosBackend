20a8c7b95c27f07ba45e338c8db42b43e1538da4
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-30 12:50:07 -0600

    feat: configurando models

---
+++ WebApiEmpleados/Models/EmpleadosDbcrudContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEmpleados.Models;
+
+public partial class EmpleadosDbcrudContext : DbContext
+{
+    public EmpleadosDbcrudContext()
+    {
+    }
+
+    public EmpleadosDbcrudContext(DbContextOptions<EmpleadosDbcrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__Empleado__CE6D8B9E98E6C105");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
